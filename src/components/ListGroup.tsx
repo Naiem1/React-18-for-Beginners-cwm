d8ab27640f18314b77e6e8ab4ba598c8f5f28185
Naiem1 <naiem4288@gmail.com>
2023-11-10 15:22:54 +0600

    added event  handler onclicked

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,19 @@
+import { MouseEvent } from "react";
+
 const ListGroup = () => {
-  let items = ['New York', 'San Francisco', 'London', 'Tokyo', 'Paris'];
-  items = [];
+  const items = ['New York', 'San Francisco', 'London', 'Tokyo', 'Paris'];
+  // items = [];
+
 
   const message = items.length === 0 ? <p>No Item Found!</p> : null;
   const getMessage = () => {
     return items.length === 0 ? <p>Item Not Found</p> : null;
   };
+
+
+  const handleClick = (event:MouseEvent) => {
+    console.log(event.target)
+  }
   return (
     <>
       <h1>List</h1>
@@ -15,7 +23,7 @@ const ListGroup = () => {
         {items.length === 0 ? <p>Item not Found</p> : null}
         {items.length === 0 && <p>Item Not Found!</p>}
         {items.map((item) => (
-          <li key={item} className="list-group-item">
+          <li key={item} className="list-group-item active" onClick={handleClick}>
             {item}
           </li>
         ))}
