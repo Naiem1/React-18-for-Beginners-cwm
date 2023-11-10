ed24a73f51fead1d0ce615378ab63d85015f16b6
Naiem1 <naiem4288@gmail.com>
2023-11-10 20:25:51 +0600

    14-showing an alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,14 +1,21 @@
-
 import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert: React.FC<Props> = ({ children }) => {
+const Alert: React.FC<Props> = ({ children, onClose }) => {
   return (
-    <div className="alert alert-primary" role="alert">
+    <div className="alert alert-primary alert-dismissible" role="alert">
       {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
     </div>
   );
 };
