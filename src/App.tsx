37e0fb8e37548ecb0b1b1024abc56551fbbd8411
Naiem1 <naiem4288@gmail.com>
2023-11-11 13:53:06 +0600

    5.6-managing-component-state: updating array of object

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,20 @@
 import { useState } from 'react';
-import Message from './components/Message';
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: 'John',
-    },
-  });
-  const [pizza, setPizza] = useState({
-    nam: 'Spicy Pepperoni',
-    toppings: ['Mushroom'],
-  })
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: 'Product 1', quantity: 1 },
-      { id: 2, title: 'Product 2', quantity: 1 },
-    ],
-  });
+  const [bugs, setBugs] = useState([
+    { id: 1, title: 'bug 1', fixed: false },
+    { id: 2, title: 'bug 2', fixed: false },
+  ]);
 
-  const handleClickGame = () => {
-    setCart({ ...game, player: {...game.player, name: 'Doe'}})
-  }
 
-  const handleClickPizza = () => {
-    setPizza({...pizza, toppings: [...pizza.toppings, 'Cheese']})
-  }
 
 
   const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
+    setBugs(bugs.map(bug => bug.id === 1 ? { ...bug, fixed: true } : bug));
   };
   return (
     <div>
       <button onClick={handleClick}>Update</button>
-      <Message/>
     </div>
   );
 }
