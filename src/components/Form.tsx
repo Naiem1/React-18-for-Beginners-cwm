27ef07d2aa2b996a9c8ca5a9010e0b45670e86ce
Naiem1 <naiem4288@gmail.com>
2023-11-11 20:19:20 +0600

    6.3-building-form: accessing form input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,39 @@
+import { FormEvent, useRef } from 'react';
+
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const person = {
+      name: '',
+      age: 0,
+    }
+
+    if (nameRef.current !== null) {
+      person.name = nameRef.current.value;
+    }
+    if (ageRef.current !== null) {
+      person.age = +ageRef.current.value;
+    }
+
+    console.log(person);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
