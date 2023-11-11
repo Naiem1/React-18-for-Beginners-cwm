279362b35ad13f47b5f907ab55b8b3d33615d5be
Naiem1 <naiem4288@gmail.com>
2023-11-11 13:29:38 +0600

    5.4-managing-component: updating state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
 import { useState } from 'react';
 
 function App() {
-  const [person, setPerson] = useState({
-    firstName: '',
-    lastName: '',
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: 'John',
+    },
   });
-  const [isLoading, setIsLoading] = useState(false);
-  return <div></div>;
+  const [pizza, setPizza] = useState({
+    nam: 'Spicy Pepperoni',
+    toppings: ['Mushroom'],
+  })
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: 'Product 1', quantity: 1 },
+      { id: 2, title: 'Product 2', quantity: 1 },
+    ],
+  });
+
+  const handleClickGame = () => {
+    setCart({ ...game, player: {...game.player, name: 'Doe'}})
+  }
+
+  const handleClickPizza = () => {
+    setPizza({...pizza, toppings: [...pizza.toppings, 'Cheese']})
+  }
+
+
+  const handleClick = () => {
+    setCart({
+      ...cart,
+      items: cart.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>Update</button>
+    </div>
+  );
 }
 
 export default App;
