55a546750cdda33dd4730cc2fc68e48d07441983
Naiem1 <naiem4288@gmail.com>
2023-11-11 15:05:35 +0600

    5.9-managing-component-state: updating nested object

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
-import {produce} from 'immer';
 import { useState } from 'react';
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: 'bug 1', fixed: false },
-    { id: 2, title: 'bug 2', fixed: false },
-    { id: 3, title: 'bug 3', fixed: false },
-  ]);
+  const [customer, setCustomer] = useState({
+    name: 'John',
+    address: {
+      city: 'San Francisco',
+      zipCode: 94111,
+    },
+  });
 
   const handleClick = () => {
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-
-        if (bug) bug.fixed = true;
-      })
-    );
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 11111 },
+    });
   };
   return (
     <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? 'Fixed' : 'new'}
-        </p>
-      ))}
       <button onClick={handleClick}>Update</button>
     </div>
   );
