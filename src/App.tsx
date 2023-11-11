b0f4e7404326c1ae69ab01af7a6b55022d040ead
Naiem1 <naiem4288@gmail.com>
2023-11-11 18:23:43 +0600

    5.10-managing-component-state: bulding expandable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Expandable from './components/ExpandableText';
 
 function App() {
   const [customer, setCustomer] = useState({
@@ -17,7 +18,20 @@ function App() {
   };
   return (
     <div>
-      <button onClick={handleClick}>Update</button>
+      {/* <button onClick={handleClick}>Update</button> */}
+      <Expandable maxChars={20}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, tempore
+        magnam exercitationem, voluptatem vel iste, quas quisquam molestiae
+        repellat obcaecati expedita! Consequatur placeat facere sint eius neque
+        est quae quam, quod praesentium possimus expedita ipsa animi ullam
+        deleniti maiores, itaque dolorem tempore ab in? Sint eligendi
+        repellendus, sapiente expedita dignissimos, praesentium temporibus vitae
+        quos harum suscipit eius quia iure aperiam totam sunt! Quod, doloremque
+        inventore nihil at dolores deleniti minima culpa iure porro praesentium
+        eligendi, modi nam ratione reprehenderit, recusandae vel laborum
+        impedit. Vitae sapiente earum aut dignissimos adipisci magnam voluptatum
+        non veritatis velit sequi nesciunt, illo officiis cumque nihil.
+      </Expandable>
     </div>
   );
 }
